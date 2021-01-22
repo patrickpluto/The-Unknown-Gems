f6e9adad202e79c28ea0e647558f9ba8fbd615a7
patrickpluto <65828188+patrickpluto@users.noreply.github.com>
2021-01-22 21:46:03 +0100

    Dayentium added

---
+++ src/main/java/net/mcreator/theunknowngems/item/DayentiumShovelItem.java
@@ -0,0 +1,52 @@
+
+package net.mcreator.theunknowngems.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ShovelItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.theunknowngems.TheUnknownGemsModElements;
+
+@TheUnknownGemsModElements.ModElement.Tag
+public class DayentiumShovelItem extends TheUnknownGemsModElements.ModElement {
+	@ObjectHolder("the_unknown_gems:dayentium_shovel")
+	public static final Item block = null;
+	public DayentiumShovelItem(TheUnknownGemsModElements instance) {
+		super(instance, 10);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ShovelItem(new IItemTier() {
+			public int getMaxUses() {
+				return 250;
+			}
+
+			public float getEfficiency() {
+				return 6f;
+			}
+
+			public float getAttackDamage() {
+				return 0f;
+			}
+
+			public int getHarvestLevel() {
+				return 2;
+			}
+
+			public int getEnchantability() {
+				return 14;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(DayentiumItem.block, (int) (1)));
+			}
+		}, 1, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
+		}.setRegistryName("dayentium_shovel"));
+	}
+}
